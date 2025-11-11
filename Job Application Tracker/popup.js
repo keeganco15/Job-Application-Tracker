d943ad368e8bde8be514034f8c464a5ce16ebe3a
Keegan Doherty <82522283+keeganco15@users.noreply.github.com>
2025-11-11 22:43:09 +0000

    Delete Job Application Tracker directory

--- Job Application Tracker/popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("openDashboard").addEventListener("click", () => {
-    const dashboardUrl = chrome.runtime.getURL("dashboard.html");
-    chrome.tabs.create({ url: dashboardUrl });
-  });
-  
